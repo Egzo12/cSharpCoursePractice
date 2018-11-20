87f1e2189e512742a5f53b3fd9c7aadb71a26799
Mindaugas Pipiras <mindaugas.pipiras@yahoo.de>
2018-11-20 13:05:37 +0200

    MagicNumberx1

--- practiceArrays/practiceArrays/Program.cs
+++ practiceArrays/practiceArrays/Program.cs
@@ -1,39 +1,134 @@
 ﻿using System;
 
 
-class Program
+namespace ArraynamuDarbai
 {
-    static void Main()
+    class Program
     {
-        findMagicNumber();
-        //printMineSweeperNumbers();
-
-
-        string[,] names;
-
-        int[][] scores;
-
-         = 8;
-
-        Console.WriteLine(scores[0][2]);
-
-        int[] numbers = new int[] { 12, 98, -12, 65, 12 };
-        foreach (int item in numbers)
+        static void Main(string[] args)
         {
-            Console.Write(item + " ");
+
+            //Starting number
+            int skaicius = 100000;
+
+            //initial values
+            int[] Converted = new int[6];
+            bool isMagic = false;
+            // loop throuth numbers starting from 100 000
+            do
+            {
+                //Converting number to Array
+                Converted = Masyvas(skaicius);
+
+                //Check for dublicates, return true if found
+                bool Hasdubs = CheckForDubs(Converted);
+
+                if (Hasdubs)
+                {
+                    Console.WriteLine("Array has dubs, skipping " + skaicius);
+                    //increment the starting number and continue
+                    skaicius++;
+                    continue;
+                }
+                else
+                {
+                    // if number has no dublicates, try to check for magic number, returns true if found
+                    bool IsMagic = MagicNumber(Converted, skaicius);
+
+                    if (IsMagic)
+                    {
+                        IsMagic = true;
+                        Console.WriteLine("Magic number found: " + skaicius);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wrong number, continue " + skaicius);
+                        //if isMagic return false, increment number and continue
+                        skaicius++;
+                    }
+                }
+
+            } while (isMagic == false);
+            // to see output
+            Console.ReadLine();
         }
-        Console.WriteLine();
-    }
 
-    private static void findMagicNumber()
-    {
-        throw new NotImplementedException("TODO: Suraskite magiška 6 skaitmenų skaičių (naudodami masyvus)!");
-    }
 
-    private static void printMineSweeperNumbers()
-    {
-        throw new NotImplementedException("TODO: Sukurkite programą kuri liepia vartotojui įvesti MineSweeper žaidimo lauko duomenis. " +
-            "Tada paskaičiuoja ir išspausdina lentelę su skaičiais kiek aplinkui(8 kryptyse įskaitant įstrižai) yra minų.");
+        private static int[] Masyvas(int number)
+        {
+            int[] masyvas = new int[6];
+            string numberis = Convert.ToString(number);
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                masyvas[i] = Convert.ToInt32(numberis.Substring(i, 1));
+            }
+            return masyvas;
+        }
+
+        private static bool CheckForDubs(int[] masyvas)
+        {
+            bool HasDubs = false;
+            for (int i = 0; i < masyvas.Length; i++)
+            {
+                for (int j = 1; j < masyvas.Length; j++)
+                {
+                    if (masyvas[i] == masyvas[j] && i != j)
+                    {
+                        HasDubs = true;
+                        break;
+                    }
+                    else
+                    {
+                        HasDubs = false;
+                        continue;
+                    }
+                }
+            }
+            return HasDubs;
+        }
+
+        private static bool MagicNumber(int[] masyvas, int num)
+        {
+            bool IsMagic = false;
+
+            int num2 = num * 2;
+            int num3 = num * 3;
+            int num4 = num * 4;
+            int num5 = num * 5;
+            int num6 = num * 6;
+
+            int[] ArrNum2 = new int[6];
+            int[] ArrNum3 = new int[6];
+            int[] ArrNum4 = new int[6];
+            int[] ArrNum5 = new int[6];
+            int[] ArrNum6 = new int[6];
+
+            ArrNum2 = Masyvas(num2);
+            ArrNum3 = Masyvas(num3);
+            ArrNum4 = Masyvas(num4);
+            ArrNum5 = Masyvas(num5);
+            ArrNum6 = Masyvas(num6);
+
+            Array.Sort<int>(masyvas);
+            Array.Sort<int>(ArrNum2);
+            Array.Sort<int>(ArrNum3);
+            Array.Sort<int>(ArrNum4);
+            Array.Sort<int>(ArrNum5);
+            Array.Sort<int>(ArrNum6);
+
+            //Ugly
+            if (masyvas[0] == ArrNum2[0] && masyvas[0] == ArrNum3[0] && masyvas[0] == ArrNum4[0] && masyvas[0] == ArrNum5[0] && masyvas[0] == ArrNum6[0])
+            {
+                if ((masyvas[1] == ArrNum2[1] && masyvas[1] == ArrNum3[1] && masyvas[1] == ArrNum4[1] && masyvas[1] == ArrNum5[1] && masyvas[1] == ArrNum6[1]))
+                {
+                    if ((masyvas[5] == ArrNum2[5] && masyvas[5] == ArrNum3[5] && masyvas[5] == ArrNum4[5] && masyvas[5] == ArrNum5[5] && masyvas[5] == ArrNum6[5]))
+                    {
+                        IsMagic = true;
+                    }
+                }
+            }
+            return IsMagic;
+        }
     }
 }
-
